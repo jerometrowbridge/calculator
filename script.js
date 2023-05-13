da52620a399474b6844b7d67ca02ec17d1c6ec74
Jerome Trowbridge <122302999+jerometrowbridge@users.noreply.github.com>
2023-05-12 23:46:35 -0500

    Add js to enable calc to perform incl multiple operations

--- script.js
+++ script.js
@@ -22,26 +22,30 @@ function divide(a, b) {
 let num1
 let num2
 let operator
-let result
+let result = null;
 let currentValue = '';
 let previousOperator = null;
 let firstOperand = '';
 let secondOperand = '';
+let oper = '';
+
 
 
 function operate(operator, num1, num2) {
     
-    if (operator == "add") {
+    if (operator == "+") {
         result = add(num1, num2);
     }
-    if (operator == "subtract") {
+    else if (operator == "-") {
         result = subtract(num1, num2);
-    }if (operator == "multiply") {
+    } else if (operator == "*") {
         result = multiply(num1, num2);
-    }if (operator == "divide") {
+    } else if (operator == "/") {
         result = divide(num1, num2);
+    } else {
+        result = "Invalid operator";
     }
-    return result;
+    return result
 }
 
 const numButtons = document.querySelectorAll('.number-buttons');
@@ -49,13 +53,20 @@ numButtons.forEach((button => {
     button.addEventListener('click', () => {
     let buttonContent = button.innerText;
     if (previousOperator == null) {
-        currentValue += buttonContent;
+        firstOperand += buttonContent;
+        display.textContent = firstOperand;
     }
+    else if (result != null) {
+        firstOperand = result;
+        secondOperand += buttonContent;
+        display.textContent = secondOperand;
+    }
+    
     else {
-        currentValue = buttonContent;
-        previousOperator = null;
+        secondOperand += buttonContent;
+        display.textContent = secondOperand;
     }
-    display.textContent = currentValue;
+    
 })
 }))
 
@@ -65,12 +76,61 @@ operButtons.forEach((button => {
         let buttonContent = button.innerText;
         previousOperator = button.innerText;
         display.textContent = buttonContent;
+        
     })
 }))
 
 const display = document.querySelector(".display");
+display.textContent = 0;
 console.log(display);
-function updateDisplay() {
-    display.textContent = numButtons.innerHTML;
+// function updateDisplay() {
+//     display.textContent = numButtons.innerHTML;
+// }
+
+// function handleNumberButtonClick(button) {
+//     if (oper === '') {
+//         firstOperand += button.innerText;
+//         updateDisplay(firstOperand);
+//     } else {
+//         secondOperand += button.innerText;
+//         updateDisplay(secondOperand);
+//     }
+// }
+
+function handleOperatorButtonClick(button) {
+    oper = button.innerText;
+    updateDisplay(oper);
 }
 
+const equal = document.querySelector(".equal-button");
+equal.addEventListener('click', () => {
+    // operate(previousOperator, parseFloat(firstOperand), parseFloat(secondOperand));
+    num1 = parseFloat(firstOperand);
+    num2 = parseFloat(secondOperand);
+    operator = previousOperator;
+    result = operate(operator, num1, num2);
+    display.textContent = result;
+    secondOperand = '';
+    console.log(result);
+    console.log(previousOperator);
+    console.log(firstOperand);
+    console.log(secondOperand);
+})
+
+// function handleEqualButtonClick() {
+//     console.log('test');
+//     num1 = firstOperand;
+//     num2 = secondOperand;
+//     operator = str(previousOperator);
+//     result = operate(operator, num1, num2);
+//     console.log(result);
+
+// }
+
+const clear = document.querySelector(".clear-button")
+clear.addEventListener('click', () => {
+    firstOperand = '';
+    secondOperand = '';
+    previousOperator = null;
+    display.textContent = 0;
+})
